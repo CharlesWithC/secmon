21432d30c743c7c0fba23ea6e23b6bd58712549e
CharlesWithC <me@charlws.com>
2026-04-27 21:52:50 -0400

    init: base telegram bot with frankenstein

--- telegram/src/main.rs
+++ telegram/src/main.rs
@@ -1,3 +1,18 @@
+use frankenstein::TelegramApi;
+use frankenstein::client_ureq::Bot;
+use frankenstein::methods::SendMessageParams;
+use secmon::utils::get_env_var_strict;
+
 fn main() {
-    println!("Hello, world!");
+    let token = get_env_var_strict::<String>("TELEGRAM_BOT_TOKEN", None);
+    let user_id = get_env_var_strict::<i64>("TELEGRAM_USER_ID", None);
+
+    let bot = Bot::new(token.as_str());
+
+    let send_message_params = SendMessageParams::builder()
+        .chat_id(user_id)
+        .text("Hello Telegram")
+        .build();
+    let result = bot.send_message(&send_message_params);
+    println!("{:?}", result);
 }
